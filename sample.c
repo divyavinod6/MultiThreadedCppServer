69ee722b4cdce76075696ebf95c453e9155ded3e
divyavinod6 <thisisdivyavinod@gmail.com>
2025-03-24 23:53:45 +0530

    Pthread is still not working

--- sample.c
+++ sample.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<unistd.h>
 #include <pthread.h>
 
-void * sample(){
-    printf("Thread sample.\n");
-    sleep(1);
-    printf("End Thread.\n");
+
+void* thread_function(void* arg) {
+    printf("Hello from thread!\n");
+    return NULL;
 }
 
 int main(void){
+    /*
     pthread_t tid,tid_1;
     pthread_create(&tid,NULL,&sample,NULL);
     pthread_create(&tid_1,NULL,&sample,NULL);
     pthread_join(tid,NULL);
     pthread_join(tid_1,NULL);
-
+    
     return 0;
+    */
+
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, thread_function, NULL)) {
+        printf("Error creating thread\n");
+        return 1;
+    }
+    pthread_join(thread, NULL);
+    return 0;
+
 }
